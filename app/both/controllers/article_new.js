147717aa7a06db661a7b070fef1d7d380f419488
Chris Mather <mather.chris@gmail.com>
2014-09-15 17:55:53 -0700

    add controller event handler

--- app/both/controllers/article_new.js
+++ app/both/controllers/article_new.js
@@ -1,1 +1,24 @@
-ArticleNewController = RouteController.extend({});
+ArticleNewController = RouteController.extend({
+  sayHello: function () {
+    return 'hello world';
+  }
+});
+
+ArticleNewController.events({
+  'submit form#new-article': function (e, tmpl) {
+    e.preventDefault();
+
+    var form = tmpl.find('form');
+    var title = tmpl.find('input[name=title]').value;
+    var body = tmpl.find('textarea[name=body]').value;
+
+    Articles.insert({
+      title: title,
+      body: body
+    }, function (err, res) {
+      if (!err) {
+        Router.go('home', {}, {query: 'q=s', hash: 'hashfrag'}); 
+      }
+    });
+  }
+});
